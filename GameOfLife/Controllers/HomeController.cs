c5eb5b4067f4b09ff227858698e67e82c22ffd1c
Cristian Thompson <cristian.thompson@jnctech.ca>
2017-11-22 11:35:45 -0400

    Add on click build new grid
    TODO: Add tick function that applies rules to grid instead of creating new grid on click

--- GameOfLife/Controllers/HomeController.cs
+++ GameOfLife/Controllers/HomeController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GameOfLife.Models;
 
 namespace GameOfLife.Controllers
 {
     public class HomeController : Controller
     {
+        Game game = new Game();
         public ActionResult Index()
         {
-            Game game = new Game();
+            return View("Index", game.grid);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateGrid()
+        {
+            Game resetGrid = new Game();
+            game.grid = resetGrid.grid;
             return View("Index", game.grid);
         }
     }
